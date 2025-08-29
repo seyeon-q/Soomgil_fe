455c4a0a0ae7ac2231efcfd7874df2e2e951c724
seyeon-q <kelly0458@naver.com>
2025-08-30 07:54:01 +0900

    완료

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import SetupPage from "./pages/SetupPage.jsx";
 import LoadingPage from "./pages/LoadingPage";
 import ResultPage from "./pages/ResultPage.jsx";
-import LoginPage from "./pages/LoginPage.jsx";
 import MyPage from "./pages/MyPage.jsx";
 import CustomWalkPage from "./pages/CustomWalkPage";
 import CustomLoadingPage from "./pages/CustomLoadingPage";
@@ -29,7 +28,6 @@ export default function App() {
       <Route path="/" element={<SetupPage />} />
       <Route path="/loading" element={<LoadingPage />} />
       <Route path="/result" element={<ResultPage />} />
-      <Route path="/login" element={<LoginPage />} />
       <Route path="/mypage" element={<MyPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
       <Route path="/custom-walk" element={<CustomWalkPage />} />
